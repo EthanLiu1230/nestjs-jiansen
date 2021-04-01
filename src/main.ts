cbe90deb8d2d26d89f1e818f17318549c6de0132
YuxLiu <liuyuxuan1230@outlook.com>
2021-04-01 12:51:45 +0800

    Config Swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configSwagger } from '../swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  configSwagger(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
---
+++ swagger.config.ts
@@ -0,0 +1,15 @@
+// swagger config
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+const config = new DocumentBuilder()
+  .setTitle('Title')
+  .setDescription('Description')
+  .setVersion('1.0')
+  .addTag('Api tag')
+  .build();
+
+export const configSwagger = (app: INestApplication) => {
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+};
